1b28ad3000a75422e0a3747d2cbbd7ff4b993f1f
yessGlory <ozgurkurucan337@gmail.com>
2021-04-15 21:08:37 +0300

    0.0.3

--- src/result.js
+++ src/result.js
@@ -1,9 +1,9 @@
 import Astroturf from "./Observer/Astroturf.js";
 import Search from "./Observable/Search.js";
 import Filter from "./Observable/Filter.js";
-<<<<<<< HEAD
+
 import { girisKontrol } from "./kullanici-giris.js";
-=======
+
 window.onload = function () {
     var user;
     if (localStorage.getItem("auth-user") != null) {
@@ -16,129 +16,130 @@ window.onload = function () {
         }
 
         var kullanicilinkleri = document.getElementsByClassName("kullanici-linkleri");
->>>>>>> 2d08172bcfd4bcbed36ee7c276dbab0a1018c9c4
 
-window.onload = function () {
 
-<<<<<<< HEAD
-    girisKontrol();
-=======
-        for (var i = 0; i < kullanicilinkleri.length; i++) {
-            kullanicilinkleri[i].style.cssText = 'display:none !important;';
+        window.onload = function () {
+
+
+            girisKontrol();
+
+            for (var i = 0; i < kullanicilinkleri.length; i++) {
+                kullanicilinkleri[i].style.cssText = 'display:none !important;';
+            }
+
+            window.location.replace("../index.html");
         }
 
-        window.location.replace("../index.html");
-    }
->>>>>>> 2d08172bcfd4bcbed36ee7c276dbab0a1018c9c4
 
-    var search = new Search();
+        var search = new Search();
 
 
-    var oReq = new XMLHttpRequest();
-    oReq.onreadystatechange = function () {
-        if (oReq.readyState == XMLHttpRequest.DONE) {
-            //console.log(oReq.response);
-            var response = oReq.response;
-            response = JSON.parse(response);
-            console.log(response);
+        var oReq = new XMLHttpRequest();
+        oReq.onreadystatechange = function () {
+            if (oReq.readyState == XMLHttpRequest.DONE) {
+                //console.log(oReq.response);
+                var response = oReq.response;
+                response = JSON.parse(response);
+                console.log(response);
 
 
-            response.forEach(element => {
-                var saha = new Astroturf(element.sahaadi, "card", element.aciklama, "88 TL", element.resim1, element.resim2, element.sehir + "/" + element.ilce, element.ozellikler);
-                saha.render();
-                search.addPostOnList(saha);
-                console.log(element.sahaadi);
-            });
-
-
-        }
-    }
-    oReq.open("GET", "../backend/sahalari-getir.php");
-    oReq.send();
-
-    var filterList = [];
-
-
-
-
-
-    var req = new XMLHttpRequest();
-    req.onreadystatechange = function () {
-        if (req.readyState == XMLHttpRequest.DONE) {
-            //console.log(oReq.response);
-            var response = req.response;
-            response = JSON.parse(response);
-            console.log(response);
-
-
-            response.forEach(element => {
-
-                var filter = new Filter(element.ozellikadi);
-
-                filter.render();
-
-                var cards = search.Observers;
-
-                //console.log("CARDS İÇERİĞİ : " + cards);
-                cards.forEach(element => {
-                    console.log("Nesne : " + element.name);
-                    filter.addPostOnList(element);
+                response.forEach(element => {
+                    var saha = new Astroturf(element.sahaadi, "card", element.aciklama, "88 TL", element.resim1, element.resim2, element.sehir + "/" + element.ilce, element.ozellikler);
+                    saha.render();
+                    search.addPostOnList(saha);
+                    console.log(element.sahaadi);
                 });
 
 
-                filterList.push(filter);
+            }
+        }
+        oReq.open("GET", "../backend/sahalari-getir.php");
+        oReq.send();
 
+        var filterList = [];
+
+
+
+
+
+        var req = new XMLHttpRequest();
+        req.onreadystatechange = function () {
+            if (req.readyState == XMLHttpRequest.DONE) {
+                //console.log(oReq.response);
+                var response = req.response;
+                response = JSON.parse(response);
+                console.log(response);
+
+
+                response.forEach(element => {
+
+                    var filter = new Filter(element.ozellikadi);
+
+                    filter.render();
+
+                    var cards = search.Observers;
+
+                    //console.log("CARDS İÇERİĞİ : " + cards);
+                    cards.forEach(element => {
+                        console.log("Nesne : " + element.name);
+                        filter.addPostOnList(element);
+                    });
+
+
+                    filterList.push(filter);
+
+                });
+
+
+            }
+        }
+        req.open("GET", "../backend/ozellikleri-getir.php");
+        req.send();
+
+
+
+        var filters = document.getElementById("sabit-filtreler");
+
+        filters.addEventListener("click", function (e) {
+
+            if (e.target.tagName == "button") {
+                e.target.backgroundColor = "#6583FE";
+            }
+
+            filterList.forEach(f => {
+                if (f.filterName == e.target.textContent) {
+                    f.updateList(e.target.textContent);
+
+                    console.log("Filtereler : => " + f.filterName);
+                }
             });
 
+        })
 
-        }
+        // for (var i = 0; i < filters.length; i++) {
+        //     filters[i].addEventListener("click", function () {
+        //         alert();
+
+        //         filterList.forEach(el => {
+        //             if (el.filterName == filters[i].textContent) {
+        //                 alert("aha buldum!");
+        //             }
+        //         });
+        //     });
+        // }
+
+
+        //Arama İnputu ile arama
+
+        var searchInput = document.getElementById("search-input");
+
+        searchInput.addEventListener('keyup', function (e) {
+            search.notify(searchInput.value);
+        })
+
+
+
+
     }
-    req.open("GET", "../backend/ozellikleri-getir.php");
-    req.send();
-
-
-
-    var filters = document.getElementById("sabit-filtreler");
-
-    filters.addEventListener("click", function (e) {
-
-        if (e.target.tagName == "button") {
-            e.target.backgroundColor = "#6583FE";
-        }
-
-        filterList.forEach(f => {
-            if (f.filterName == e.target.textContent) {
-                f.updateList(e.target.textContent);
-
-                console.log("Filtereler : => " + f.filterName);
-            }
-        });
-
-    })
-
-    // for (var i = 0; i < filters.length; i++) {
-    //     filters[i].addEventListener("click", function () {
-    //         alert();
-
-    //         filterList.forEach(el => {
-    //             if (el.filterName == filters[i].textContent) {
-    //                 alert("aha buldum!");
-    //             }
-    //         });
-    //     });
-    // }
-
-
-    //Arama İnputu ile arama
-
-    var searchInput = document.getElementById("search-input");
-
-    searchInput.addEventListener('keyup', function (e) {
-        search.notify(searchInput.value);
-    })
-
-
-
 
 }
-
