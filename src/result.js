73a87cff0597df7833ed3dfef15ef9125c6181d6
yessGlory <ozgurkurucan337@gmail.com>
2021-04-15 22:38:05 +0300

    bug fixed

--- src/result.js
+++ src/result.js
@@ -1,169 +1,145 @@
 import Astroturf from "./Observer/Astroturf.js";
 import Search from "./Observable/Search.js";
 import Filter from "./Observable/Filter.js";
+
 import { girisKontrol } from "./kullanici-giris.js";
 
 window.onload = function () {
+    var user;
+    if (localStorage.getItem("auth-user") != null) {
+        user = localStorage.getItem("auth-user");
 
-    girisKontrol();
+        console.log("Giriş yapan kullanıcı : " + user);
+        var girislinkleri = document.getElementsByClassName("giris-linkleri");
+        for (var i = 0; i < girislinkleri.length; i++) {
+            girislinkleri[i].style.display = 'none';
+        }
 
-    var filterList = [];
-
-
-
-
-    response.forEach(element => {
-        var saha = new Astroturf(element.sahaadi, "card", element.aciklama, "88 TL", element.resim1, element.resim2, element.sehir + "/" + element.ilce, element.ozellikler);
-        saha.render();
-        search.addPostOnList(saha);
-        console.log(element.sahaadi);
-    });
-
-    var req = new XMLHttpRequest();
-    req.onreadystatechange = function () {
-        if (req.readyState == XMLHttpRequest.DONE) {
-            //console.log(oReq.response);
-            var response = req.response;
-            response = JSON.parse(response);
-            console.log(response);
+        var kullanicilinkleri = document.getElementsByClassName("kullanici-linkleri");
 
 
-            response.forEach(element => {
+        window.onload = function () {
 
-                var filter = new Filter(element.ozellikadi);
 
-                filter.render();
+            girisKontrol();
 
-                var cards = search.Observers;
+            for (var i = 0; i < kullanicilinkleri.length; i++) {
+                kullanicilinkleri[i].style.cssText = 'display:none !important;';
+            }
 
-                //console.log("CARDS İÇERİĞİ : " + cards);
-                cards.forEach(element => {
-                    console.log("Nesne : " + element.name);
-                    filter.addPostOnList(element);
+            window.location.replace("../index.html");
+        }
+
+
+        var search = new Search();
+
+
+        var oReq = new XMLHttpRequest();
+        oReq.onreadystatechange = function () {
+            if (oReq.readyState == XMLHttpRequest.DONE) {
+                //console.log(oReq.response);
+                var response = oReq.response;
+                response = JSON.parse(response);
+                console.log(response);
+
+
+                response.forEach(element => {
+                    var saha = new Astroturf(element.sahaadi, "card", element.aciklama, "88 TL", element.resim1, element.resim2, element.sehir + "/" + element.ilce, element.ozellikler);
+                    saha.render();
+                    search.addPostOnList(saha);
+                    console.log(element.sahaadi);
                 });
 
 
-                filterList.push(filter);
-
-            });
-
-
+            }
         }
-    }
-    req.open("GET", "../backend/ozellikleri-getir.php");
-    req.send();
+        oReq.open("GET", "../backend/sahalari-getir.php");
+        oReq.send();
 
-    var filterList = [];
-
+        var filterList = [];
 
 
 
 
-    var req = new XMLHttpRequest();
-    req.onreadystatechange = function () {
-        if (req.readyState == XMLHttpRequest.DONE) {
-            //console.log(oReq.response);
-            var response = req.response;
-            response = JSON.parse(response);
-            console.log(response);
+
+        var req = new XMLHttpRequest();
+        req.onreadystatechange = function () {
+            if (req.readyState == XMLHttpRequest.DONE) {
+                //console.log(oReq.response);
+                var response = req.response;
+                response = JSON.parse(response);
+                console.log(response);
 
 
-            response.forEach(element => {
+                response.forEach(element => {
 
-                var filter = new Filter(element.ozellikadi);
+                    var filter = new Filter(element.ozellikadi);
 
-                filter.render();
+                    filter.render();
 
-                var cards = search.Observers;
+                    var cards = search.Observers;
 
-                //console.log("CARDS İÇERİĞİ : " + cards);
-                cards.forEach(element => {
-                    console.log("Nesne : " + element.name);
-                    filter.addPostOnList(element);
+                    //console.log("CARDS İÇERİĞİ : " + cards);
+                    cards.forEach(element => {
+                        console.log("Nesne : " + element.name);
+                        filter.addPostOnList(element);
+                    });
+
+
+                    filterList.push(filter);
+
                 });
 
 
-                filterList.push(filter);
+            }
+        }
+        req.open("GET", "../backend/ozellikleri-getir.php");
+        req.send();
 
+
+
+        var filters = document.getElementById("sabit-filtreler");
+
+        filters.addEventListener("click", function (e) {
+
+            if (e.target.tagName == "button") {
+                e.target.backgroundColor = "#6583FE";
+            }
+
+            filterList.forEach(f => {
+                if (f.filterName == e.target.textContent) {
+                    f.updateList(e.target.textContent);
+
+                    console.log("Filtereler : => " + f.filterName);
+                }
             });
 
+        })
 
-        }
+        // for (var i = 0; i < filters.length; i++) {
+        //     filters[i].addEventListener("click", function () {
+        //         alert();
+
+        //         filterList.forEach(el => {
+        //             if (el.filterName == filters[i].textContent) {
+        //                 alert("aha buldum!");
+        //             }
+        //         });
+        //     });
+        // }
+
+
+        //Arama İnputu ile arama
+
+        var searchInput = document.getElementById("search-input");
+
+        searchInput.addEventListener('keyup', function (e) {
+            search.notify(searchInput.value);
+        })
+
+
+
+
     }
-    req.open("GET", "../backend/ozellikleri-getir.php");
-    req.send();
-
-
-
-    var filters = document.getElementById("sabit-filtreler");
-
-    filters.addEventListener("click", function (e) {
-
-        if (e.target.tagName == "button") {
-            e.target.backgroundColor = "#6583FE";
-        }
-
-        filterList.forEach(f => {
-            if (f.filterName == e.target.textContent) {
-                f.updateList(e.target.textContent);
-
-                console.log("Filtereler : => " + f.filterName);
-            }
-        });
-
-    })
-
-    // for (var i = 0; i < filters.length; i++) {
-    //     filters[i].addEventListener("click", function () {
-    //         alert();
-
-    //         filterList.forEach(el => {
-    //             if (el.filterName == filters[i].textContent) {
-    //                 alert("aha buldum!");
-    //             }
-    //         });
-    //     });
-    // }
-
-    filters.addEventListener("click", function (e) {
-
-        if (e.target.tagName == "button") {
-            e.target.backgroundColor = "#6583FE";
-        }
-
-        filterList.forEach(f => {
-            if (f.filterName == e.target.textContent) {
-                f.updateList(e.target.textContent);
-
-                console.log("Filtereler : => " + f.filterName);
-            }
-        });
-
-    })
-
-    // for (var i = 0; i < filters.length; i++) {
-    //     filters[i].addEventListener("click", function () {
-    //         alert();
-
-    //         filterList.forEach(el => {
-    //             if (el.filterName == filters[i].textContent) {
-    //                 alert("aha buldum!");
-    //             }
-    //         });
-    //     });
-    // }
-
-
-    //Arama İnputu ile arama
-
-    var searchInput = document.getElementById("search-input");
-
-    searchInput.addEventListener('keyup', function (e) {
-        search.notify(searchInput.value);
-    })
-
-
-
 
 }
-
